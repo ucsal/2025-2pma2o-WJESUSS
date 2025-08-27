6bb7eb4684b667df0f750b590ab0740202a1d158
WJESUSS <washquesia@gmail.com>
2025-08-27 12:03:57 -0300

    Refatora código para aplicar o Princípio Aberto/Fechado (OCP)

--- src/test/java/br/com/mariojp/solid/ocp/DiscountCalculatorTest.java
+++ src/test/java/br/com/mariojp/solid/ocp/DiscountCalculatorTest.java
@@ -3,11 +3,14 @@ package br.com.mariojp.solid.ocp;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 public class DiscountCalculatorTest {
     @Test
     void partner_gets_12_percent() {
-        var calc = new DiscountCalculator();
-        assertEquals(88.0, calc.apply(100.0, CustomerType.PARTNER), 0.0001,
-            "PARTNER deveria ter 12% de desconto");
+        var calc = new DiscountCalculator(new PartnerCustomerDiscount());
+        assertEquals(88.0, calc.apply(100.0), 0.0001,
+                "PARTNER deveria ter 12% de desconto");
     }
 }
